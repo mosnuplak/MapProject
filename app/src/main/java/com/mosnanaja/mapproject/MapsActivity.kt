8b040d3b122db39aa05a405d90e583d5412ae82b
conaninw2 <se57160707@gmail,com>
2017-10-11 14:13:33 +0700

    ของเกมส์เองไงจะใครล้า

--- app/src/main/java/com/mosnanaja/mapproject/MapsActivity.kt
+++ app/src/main/java/com/mosnanaja/mapproject/MapsActivity.kt
@@ -1,31 +1,45 @@
 package com.mosnanaja.mapproject
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
-import com.google.android.gms.maps.model.CameraPosition
-
+import java.lang.NullPointerException
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,View.OnClickListener {
     override fun onClick(v: View?) {
-        when(v){
-            btnJJ -> {moveToPlaceTarget(13.805194, 100.551894,"เจเจกรีน"); Toast.makeText(this,"put move to JJ Here!!!",Toast.LENGTH_LONG).show()}
-            btnSJ -> {moveToPlaceTarget(13.809275, 100.559083, "ตึกนี้แหละ"); Toast.makeText(this,"put move to SJ Here!!!",Toast.LENGTH_LONG).show()}
-            btnSent -> {moveToPlaceTarget(13.815813, 100.560972,"เซนลาด"); Toast.makeText(this,"put move to Sent Here!!!",Toast.LENGTH_LONG).show()}
+        when (v) {
+            btnJJ -> {
+                moveToPlaceTarget(13.805194, 100.551894, "เจเจกรีน"); Toast.makeText(this, "put move to JJ Here!!!", Toast.LENGTH_LONG).show()
+            }
+            btnSJ -> {
+                moveToPlaceTarget(13.809275, 100.559083, "ตึกนี้แหละ"); Toast.makeText(this, "put move to SJ Here!!!", Toast.LENGTH_LONG).show()
+            }
+            btnSent -> {
+                moveToPlaceTarget(13.815813, 100.560972, "เซนลาด"); Toast.makeText(this, "put move to Sent Here!!!", Toast.LENGTH_LONG).show()
+            }
+            btn_goTo_maps -> {
+                val gmmIntentUri = Uri.parse("geo:13.809243,100.5589972 ?q= SJinfinityone")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.`package` = "com.google.android.apps.maps"
+                startApp(mapIntent)
+//            startActivity(mapIntent);}
+            }
         }
     }
 
-    private fun moveToPlaceTarget(Lat: Double, Lng: Double , namePlace: String) {
+    private fun moveToPlaceTarget(Lat: Double, Lng: Double, namePlace: String) {
         val place = LatLng(Lat, Lng)
         val cameraPosition = CameraPosition.Builder()
                 .target(place)      // Sets the center of the map to Mountain View
@@ -53,6 +67,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,View.OnClickListene
         btnJJ.setOnClickListener(this)
         btnSJ.setOnClickListener(this)
         btnSent.setOnClickListener(this)
+        btn_goTo_maps.setOnClickListener(this)
     }
 
     /**
@@ -71,5 +86,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,View.OnClickListene
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun startApp(mapIntent: Intent) {
+        try {
+//            var intent = packageManager.getLaunchIntentForPackage(mapIntent.toString())
+//            intent!!.addCategory(Intent.CATEGORY_LAUNCHER)
+            startActivity(mapIntent)
+        } catch (e: NullPointerException) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("market://details?id=" + mapIntent)
+            startActivity(intent)
+        }
     }
 }
